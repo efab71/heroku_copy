6db050d64f2c9b43c058ffca65fcbeea1bcfa42d
enrico <e.fabiano71@btinternet.com>
2020-01-12 13:57:53 +0000

    deleted DATABASE SSL_REQUIRE TRUE

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # ENRICO'S DETAILS 
 #DATABASES = {
