4b7373972013212eb1bfafd9676db225469f8e7d
enrico <e.fabiano71@btinternet.com>
2020-01-12 12:30:24 +0000

    modified timestamp to be an integer

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -132,3 +132,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
+
+REST_FRAMEWORK = {
+    
+    # transforms the timestam into an integer
+    'DATETIME_FORMAT': '%s', 
+}
